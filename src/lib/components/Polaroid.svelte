68891b1b6e4a081268764c55a012484d116b1241
paoloricciuti <ricciutipaolo@gmail.com>
2025-10-31 09:20:14 +0100

    fix: smaller min size

--- src/lib/components/Polaroid.svelte
+++ src/lib/components/Polaroid.svelte
@@ -49,7 +49,7 @@
 <style>
 	.polaroid {
 		position: relative;
-		width: min(300px, 90vw);
+		width: min(300px, 65vw);
 		aspect-ratio: var(--aspect-ratio);
 		background: white;
 		padding: 1rem 1rem 3rem 1rem;
